c22654a67fc58bc831b4bc7544c1ae8f5890a900
Aaron Rodi <metalhead5209@gmail.com>
2022-07-27 20:01:01 -0500

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem/ExpenseItem';
+
+function App() {
+
+  return (
+    <div className="App">
+      <ExpenseItem />
+    </div>
+  )
+}
+
+export default App
