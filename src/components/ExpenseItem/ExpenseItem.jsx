766fb746258689ef704045aa7f20fda81fc2ba10
Aaron Rodi <metalhead5209@gmail.com>
2022-07-28 19:17:19 -0500

    added item component with styling

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import './ExpenseItem.css';
 
 const ExpenseItem = () => {
   return (
-    <div>ExpenseItem</div>
-  )
-}
+    <div className="expense-item">
+      <div>July 28th 2022</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$268.27</div>
+      </div>
+    </div>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
