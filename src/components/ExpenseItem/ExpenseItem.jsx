a18ae36dd8a09d78102604176d24a216d7465008
Aaron Rodi <metalhead5209@gmail.com>
2022-07-28 20:10:46 -0500

    made component reusable by adding props

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
 
-  const expenseDate = new Date(2022, 6, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 298.63;
 
   return (
     <div className="expense-item">
-      <div> {expenseDate.toDateString()} </div>
+      <div> {props.date.toDateString()} </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
