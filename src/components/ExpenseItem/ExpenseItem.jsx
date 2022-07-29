aed914d5b93baaa163b85addbe460cf7935f2f92
Aaron Rodi <metalhead5209@gmail.com>
2022-07-28 20:59:27 -0500

    moved date in to component

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate/ExpenseDate";
 
 const ExpenseItem = (props) => {
 
 
   return (
     <div className="expense-item">
-      <div> {props.date.toDateString()} </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
