357817bb3b66fdd02b866c75e151a0f1f570316e
Aaron Rodi <metalhead5209@gmail.com>
2022-07-30 18:49:46 -0500

    added some dummy state to component

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../Card/Card";
 
 const ExpenseItem = (props) => {
 
+  const [title, setTitle] = useState(props.title);
+
+  const handleClick = () => {
+    setTitle('Poop')
+    console.log(title)
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={handleClick}>Change Title</button>
     </Card>
   );
 };
