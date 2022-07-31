b1eabdf8ae3321c9fee6c0b9c6337cf584ad7a05
Aaron Rodi <metalhead5209@gmail.com>
2022-07-31 08:18:44 -0500

    added state to input elemtemts to grab values

--- src/components/ExpenseForm/ExpenseForm.jsx
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const [ title, setTitle] = useState('');
+    const [ amount, setAmount ] = useState(0);
+    const [ date, setDate ] = useState('');
+
+    const handleTitleChange = (e) => {
+      setTitle(e.target.value);
+    };
+
+    const handleAmountChange = (e) => {
+      setAmount(e.target.value);
+    };
+
+    const handleDateChange = (e) => {
+      setDate(e.target.value);
+    };
+  
+
   return (
     <div>
-      <form action="">
+      <form action="" onSubmit={(e) => e.preventDefault()}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" />
+            <input type="text" onChange={handleTitleChange} />
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min='0.01' step='0.01S' />
+            <input type="number" min='0.01' step='0.01S' onChange={handleAmountChange} />
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="date" min='2019-01-01' max='2022-12-31' />
+            <input type="date" min='2019-01-01' max='2022-12-31' onChange={handleDateChange} />
           </div>
         </div>
         <div className="new-expense__actions">
@@ -27,4 +44,4 @@ const ExpenseForm = () => {
   );
 };
 
-export default ExpenseForm;
+export default ExpenseForm; 
